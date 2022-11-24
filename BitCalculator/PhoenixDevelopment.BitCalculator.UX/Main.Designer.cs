b4e448f4ccc1c80b963bdb03c4e2e5bf00051634
tahahanifkhan <110609652+tahahanifkhan@users.noreply.github.com>
2022-11-24 16:38:11 +0500

    Add files via upload
    This key is what your application use for encryption and decryption which you should keep secret else anyone can decrypt your data with that key and it must be either 128 bit or 192 bit (not even in between). If you don’t know that your supplied key is either 128/192 bit or not you can bit calculation.

---
+++ BitCalculator/PhoenixDevelopment.BitCalculator.UX/Main.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PhoenixDevelopment.BitCalculator.UX
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelBits = new System.Windows.Forms.Label();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.labelHex = new System.Windows.Forms.Label();
+            this.textBoxHex = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.AcceptsReturn = true;
+            this.textBoxInput.AcceptsTab = true;
+            this.textBoxInput.Location = new System.Drawing.Point(12, 25);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(510, 57);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(12, 9);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(127, 13);
+            this.labelInput.TabIndex = 1;
+            this.labelInput.Text = "String to encode (UTF-8):";
+            // 
+            // labelBits
+            // 
+            this.labelBits.AutoSize = true;
+            this.labelBits.Location = new System.Drawing.Point(12, 91);
+            this.labelBits.Name = "labelBits";
+            this.labelBits.Size = new System.Drawing.Size(55, 13);
+            this.labelBits.TabIndex = 4;
+            this.labelBits.Text = "Size (bits):";
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Location = new System.Drawing.Point(73, 88);
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.ReadOnly = true;
+            this.textBoxSize.Size = new System.Drawing.Size(449, 20);
+            this.textBoxSize.TabIndex = 5;
+            // 
+            // labelHex
+            // 
+            this.labelHex.AutoSize = true;
+            this.labelHex.Location = new System.Drawing.Point(12, 117);
+            this.labelHex.Name = "labelHex";
+            this.labelHex.Size = new System.Drawing.Size(55, 13);
+            this.labelHex.TabIndex = 6;
+            this.labelHex.Text = "Hex Data:";
+            // 
+            // textBoxHex
+            // 
+            this.textBoxHex.Location = new System.Drawing.Point(73, 114);
+            this.textBoxHex.Multiline = true;
+            this.textBoxHex.Name = "textBoxHex";
+            this.textBoxHex.ReadOnly = true;
+            this.textBoxHex.Size = new System.Drawing.Size(449, 138);
+            this.textBoxHex.TabIndex = 7;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 264);
+            this.Controls.Add(this.textBoxHex);
+            this.Controls.Add(this.labelHex);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.labelBits);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.textBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.Text = "String Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelBits;
+        private System.Windows.Forms.TextBox textBoxSize;
+        private System.Windows.Forms.Label labelHex;
+        private System.Windows.Forms.TextBox textBoxHex;
+    }
+}
+
---
+++ BitCalculator/PhoenixDevelopment.BitCalculator.UX/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PhoenixDevelopment.BitCalculator.UX
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            textBoxSize.Text = (UTF8Encoding.UTF8.GetByteCount(textBoxInput.Text)*8).ToString();
+
+            string HexText = null;
+
+            foreach (char c in textBoxInput.Text)
+            {
+                HexText += string.Format("{0:x2}", (uint)System.Convert.ToUInt32(c)) + " ";
+            }
+
+            textBoxHex.Text = HexText;
+        }
+    }
+}
